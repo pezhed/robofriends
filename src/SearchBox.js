a3292f2d91dfc0595d52c3853d09bd5c11c6613e
Eric Lopez <ericlopez07@gmail.com>
2019-03-22 06:32:40 -0600

    First commit

---
+++ src/SearchBox.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SearchBox = () => {
+  return (
+    <div>
+      <input
+      type='search'
+      placeHolder='search robots'
+      />
+    </div>
+  );
+}
+
+export default SearchBox;
